c23a8b122330d35de093b664bb3047378341b58a
BhavsarDheeraj <dheerajbhavsar940@gmail.com>
2025-03-06 20:06:59 +0530

    Refactor middleware

--- routers/tasksRouter.js
+++ routers/tasksRouter.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const { getAllTasksHandler, getTaskHandler, createTaskHandler, updateTaskHandler, deleteTaskHandler } = require('../controllers/tasksController');
-
+const { validateTaskMiddleware } = require('../middlewares/validateTaskMiddleware');
 
 const router = new express.Router();
-
-
-const validateTaskMiddleware = (req, res, next) => {
-    const { title, description, completed } = req.body;
-    if (!title) return res.status(400).send({ message: 'Invalid title provided' });
-    if (!description) return res.status(400).send({ message: 'Invalid description provided' });
-    if (typeof completed !== 'boolean') return res.status(400).send({ message: 'Invalid completed status provided' });
-    next();
-}
-
 
 router.get('/', getAllTasksHandler);
 
